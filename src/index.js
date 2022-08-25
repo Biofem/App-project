09f2e405f6e5640b754d454906fb9c46078ba37b
abiola <110936627+Biofem@users.noreply.github.com>
2022-08-25 14:41:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,13 +27,12 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-
-function displayTemperature(response) {
+function showTemperature(response){
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
+  let descriptionElement =  dodcument.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
+  let windElement = ducument.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
